68cffec7754b9d3f4a5b2850d77c62d4d3547e33
Ankush Dutt <ankushd100@gmail.com>
2020-03-10 14:24:52 +0530

    change to app from app.js in package.json

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = require('./conig.prod')    
+    module.exports = require('./config.prod')    
 } else {
     module.exports = require('./config.dev')
 }
